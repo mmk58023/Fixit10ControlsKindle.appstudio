dcc0dd5668cdd2bfb0c830c6951edaba1f517d97
Maya <mmk58023@creighton.edu>
2020-04-05 22:01:54 -0500

    describeYouForm Complete
    done

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,8 @@
+rdoDescribeYou.onclick=function(){
+  lblPersonalityType.value = `I would agree that you are a ${$("input[name=rdoDescribeYou]:checked").prop("value")} person too!`
+}
 
 
-
-
-
+btnFavExercises.onclick=function(){
+  ChangeForm(ABfavExercises)
+}
