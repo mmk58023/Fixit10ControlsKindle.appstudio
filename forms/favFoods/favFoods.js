795ea3fdd633304c8a08a005140767c77826e793
Maya <mmk58023@creighton.edu>
2020-04-05 20:53:47 -0500

    favFoods Form Complete
    done

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,24 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+
+favFoods.onshow=function(){
+  //always clear before populating
+  lstFavoriteFoods.clear()
+  //put array goodFoods into the list
+  for (i = 0; i <= goodFoods.length - 1; i++)
+      lstFavoriteFoods.addItem(goodFoods[i])
+}
+
+
+lstFavoriteFoods.onclick=function(s){
+  if (typeof(s) == "object"){
+  return
+} else {
+  let textChoice = NSB.$("lstFavoriteFoods_" + s).textContent 
+  lblMessage.value = `You picked ${textChoice} -that is a great choice!`
+}
+}
+
+btnNextPage.onclick=function(){
+  ChangeForm(dessertVoting)
+}
--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -2,32 +2,31 @@
 //In case login doesn't work
 
 btnShortcut.onclick=function(){
-  ChangeForm(favFoods)
+ ChangeForm(favFoods) 
 }
 
 //clear variables
 
 loginCU.onshow=function(){
-  inptNetID.value = ""
-  inpPassword.value = ""
+  txtNetID.value = ""
+  txtPassword.value = ""
 }
 
 btnLogin.onclick=function(){
   //Declare global variables
-
+  
 var req = ""
 
 //AJAX
 
-req = Ajax("http://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inpPassword.value)
+req = Ajax("http://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + txtNetID.value + "&j_password=" + txtPassword.value)
 
-if (req.status == 200) { //everything worked.
-    lblResponse.hidden = false
-    lblResponse.value = "Your LDAP return code was " + req.responseText
-    //1 good 0 bad
-  } else {
-    //had a problem with the AJAX request
-    lblResponse.hidden = false
-    lblResponse.value = "Error: " + req.status;
+if (req.responseText == 1) { //everything worked.
+    ChangeForm(favFoods)
+} else {
+    //had a problem with login
+    txtNetID.value = ""
+    txtPassword.value = ""
+    lblResponse.value = `Please try again. Your username or password was invalid.`;
 }
 }
